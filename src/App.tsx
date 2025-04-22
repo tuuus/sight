a4524723c96262d61e48afec35dd92b9fe3a7071
Luis Labetoulle <labetoulle.luis@gmail.com>
2025-04-22 23:38:10 +0200

    modif

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import React, { useState } from 'react';
-import Staff from './Staff';
-import Keyboard from './Keyboard';
 import NoteTrainer from './NoteTrainer';
 
 const App: React.FC = () => {
-  const [currentNote, setCurrentNote] = useState<string>('c/4');
-
-  const handleNotePlayed = (note: string) => {
-    setCurrentNote(note);
-  };
-
   return (
     <div style={{ textAlign: 'center', backgroundColor: '#f5f5f5', minHeight: '100vh', display: 'flex', flexDirection: 'column'  }}>
       <h1 style={{color: "red"}}>Lecteur de Notes 🎵</h1>
